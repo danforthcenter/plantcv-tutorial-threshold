5ff07dc215fe2fd937a5e81bae97a2fc8522e7aa
annikafischer <145135602+AFis-245@users.noreply.github.com>
2024-11-12 15:31:14 -0600

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -4,35 +4,33 @@ import pytest
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-# inputs_list = [
-#     ['modules', '01.thresholding.ipynb'],
-#     ['modules', '02.edge-detection.ipynb'],
-#     ['modules', '03.supervised-machine-learning.ipynb'],
-#     ['modules', '04.unsupervised-machine-learning.ipynb'],
-#     ['modules', '05.background-subtraction.ipynb'],
-#     ['modules', '06.2d-thresholding.ipynb']
-# ]
+project_root = os.getcwd()
 inputs_list = [
-    os.path.join('modules', '01.thresholding.ipynb')
+    [os.path.join(project_root, 'modules'), '01.thresholding.ipynb'],
+    [os.path.join(project_root, 'modules'), '02.edge-detection.ipynb'],
+    [os.path.join(project_root, 'modules'), '03.supervised-machine-learning.ipynb'],
+    [os.path.join(project_root, 'modules'), '04.unsupervised-machine-learning.ipynb'],
+    [os.path.join(project_root, 'modules'), '05.background-subtraction.ipynb'],
+    [os.path.join(project_root, 'modules'), '06.2d-thresholding.ipynb']
 ]
 
 
 # ##########################
 # Tests executing the notebook
 # ##########################
-@pytest.mark.parametrize('notebook', inputs_list)
-def test_notebook(notebook, tmpdir):
+@pytest.mark.parametrize('dir,notebook', inputs_list)
+def test_notebook(dir, notebook, tmpdir):
     tmp = tmpdir.mkdir('sub')
     # Change working directory
     #os.chdir(dir)
 
     # Open the notebook
-    with open(notebook, "r") as f:
+    with open(os.path.join(dir, notebook), "r") as f:
         nb = nbformat.read(f, as_version=4)
 
     # Process the notebook
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-    ep.preprocess(nb, {"metadata": {"path": os.getcwd()}})
+    ep.preprocess(nb, {"metadata": {"path": dir}})
 
     # Save the executed notebook
     out_nb = os.path.join(tmp, "executed_notebook.ipynb")
