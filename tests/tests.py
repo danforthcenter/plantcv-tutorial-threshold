619496f1b5520428ea1dd1b5398f4ce33e214b8b
Noah Fahlgren <noahfahlgren@gmail.com>
2025-05-06 11:03:39 -0500

    Replace with standard test

--- tests/tests.py
+++ tests/tests.py
@@ -1,36 +1,18 @@
 import os
-import shutil
-import pytest
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-project_root = os.getcwd()
-inputs_list = [
-    [os.path.join(project_root, 'modules'), '01.thresholding.ipynb'],
-    [os.path.join(project_root, 'modules'), '02.edge-detection.ipynb'],
-    [os.path.join(project_root, 'modules'), '03.supervised-machine-learning.ipynb'],
-    [os.path.join(project_root, 'modules'), '04.unsupervised-machine-learning.ipynb'],
-    [os.path.join(project_root, 'modules'), '05.background-subtraction.ipynb'],
-    [os.path.join(project_root, 'modules'), '06.2d-thresholding.ipynb']
-]
 
-
-# ##########################
-# Tests executing the notebook
-# ##########################
-@pytest.mark.parametrize('dir,notebook', inputs_list)
-def test_notebook(dir, notebook, tmpdir):
+def test_notebook(tmpdir):
+    """Test the notebook."""
     tmp = tmpdir.mkdir('sub')
-    # Change working directory
-    #os.chdir(dir)
-
     # Open the notebook
-    with open(os.path.join(dir, notebook), "r") as f:
+    with open("index.ipynb", "r") as f:
         nb = nbformat.read(f, as_version=4)
 
     # Process the notebook
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-    ep.preprocess(nb, {"metadata": {"path": dir}})
+    ep.preprocess(nb, {"metadata": {"path": os.getcwd()}})
 
     # Save the executed notebook
     out_nb = os.path.join(tmp, "executed_notebook.ipynb")
@@ -38,4 +20,3 @@ def test_notebook(dir, notebook, tmpdir):
         nbformat.write(nb, f)
 
     assert os.path.exists(out_nb)
-
